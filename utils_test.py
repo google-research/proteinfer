2f75e6224593b0e4c7a16af47fb9d87264e9a53d
Max Bileschi <mlbileschi@google.com>
2020-05-27 15:14:24 -0400

    utils.py

--- utils_test.py
+++ utils_test.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2019 The Google Research Authors.
+# Copyright 2020 The Google Research Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ from __future__ import print_function
 from absl.testing import parameterized
 import numpy as np
 import utils
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 
 class TestTensorUtils(parameterized.TestCase):
