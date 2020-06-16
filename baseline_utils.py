6fc5a4b0bd4211dd43e36f7b43b312fcdc6d2de4
Max Bileschi <mlbileschi@google.com>
2020-06-16 16:39:46 -0400

    Load blast results

---
+++ baseline_utils.py
@@ -0,0 +1,363 @@
+# coding=utf-8
+# Copyright 2020 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python2, python3
+"""Utilities for evaluating baseline implementations of function annotation."""
+
+from __future__ import absolute_import
+from __future__ import division
+
+from __future__ import print_function
+
+import re
+import typing
+from typing import Any, Dict, FrozenSet, List, Set, Text
+
+import numpy as np
+import pandas as pd
+import six
+from six.moves import zip
+import tensorflow.compat.v1 as tf
+import tqdm
+
+_BLAST_OUTPUT_COLUMNS = [
+    'query_id',
+    'subject_id',
+    'percent_seq_identity',
+    'alignment_length',
+    'n_mismatches',
+    'n_gap_opens',
+    'query_alignment_start',
+    'query_alignment_end',
+    'subject_alignment_start',
+    'subject_alignment_end',
+    'expected_value',
+    'bit_score',
+]
+
+_MERGE_COL_NAME = 'merge_col'
+
+# e.g. >accession="A0A0PX123"	labels="PF00001,PF00002"
+# Notice optional fasta header character '>' at the beginning.
+_SEQUENCE_HEADER_RE = re.compile(r'^>?accession="(\w+)"\tlabels="(.*)"')
+
+# Format of query sequence field for interproscan.
+_INTERPROSCAN_ACCESSION_REGEX = re.compile(r'accession="(\w+)"')
+
+
+def _get_sequence_name_from_sequence_header(s):
+  """Returns sequence name from fasta header.
+
+  Args:
+    s: Fasta header input string. Format of sequence headers is
+      >accession_"label1,label2". The initial '>' character is optional.
+  """
+  return _SEQUENCE_HEADER_RE.findall(s)[0][0]  # 0th match, 0th capture group.
+
+
+def _get_labels_from_sequence_header(s):
+  r"""Returns labels from fasta header.
+
+  Args:
+    s: Fasta header input string. Format of sequence headers is
+      >accession_"label1,label2". The initial '>' character is optional.
+  """
+  # 0th match, 1th capture group.
+  regex_match = _SEQUENCE_HEADER_RE.findall(s)[0][1]
+  if regex_match == '':  # pylint: disable=g-explicit-bool-comparison
+    # If the regex match is the empty string, we just want an empty set, as
+    # there are no labels.
+    return set()
+  return set(six.ensure_str(regex_match).split(','))
+
+
+class BlastResult(
+    typing.NamedTuple('BlastResult', (
+        ('sequence_name', Text),
+        ('predicted_label', Set[Text]),
+        ('percent_seq_identity', float),
+        ('e_value', float),
+        ('bit_score', float),
+    ))):
+  """Result of parsing BLAST output."""
+
+  @staticmethod
+  def from_string(s, ground_truth_lookup):
+    """Constructs a BlastResult from a line in blast tsv output."""
+    tab_split = six.ensure_str(s).split('\t')
+    query_sequence = six.ensure_str(tab_split[0]).split('"')[1]
+    closest_sequence = six.ensure_str(tab_split[1]).split('"')[1]
+
+    percent_seq_identity = float(tab_split[2])
+    e_value = float(tab_split[10])
+    bit_score = float(tab_split[11])
+
+    predicted_label = ground_truth_lookup[closest_sequence]
+
+    return BlastResult(
+        sequence_name=query_sequence,
+        predicted_label=predicted_label,
+        percent_seq_identity=percent_seq_identity,
+        e_value=e_value,
+        bit_score=bit_score)
+
+
+class InterproScanResult(
+    typing.NamedTuple('InterproScanResult', (('sequence_name', Text),
+                                             ('predicted_label', Set[Text])))):
+  """Result of parsing InterproScan output."""
+
+  @staticmethod
+  def from_string(s):
+    tab_split = six.ensure_str(s).rstrip('\n').split('\t')
+    if len(tab_split) < 14 or tab_split[13] == '':  # pylint: disable=g-explicit-bool-comparison
+      go_labels = set()
+    else:
+      go_labels = set(six.ensure_str(tab_split[13]).split('|'))
+
+    return InterproScanResult(
+        sequence_name=_INTERPROSCAN_ACCESSION_REGEX.findall(tab_split[0])[0],
+        predicted_label=go_labels)
+
+
+def load_ground_truth(filename):
+  """Returns dataframe of ground truth from FASTA file.
+
+  Documentation on how to produce this FASTA file is available at
+  https://docs.google.com/document/d/1uB8J_a1cURIeVNqa5SOJRBBtimI1nOHbU1NbN43Xr0s
+
+  Args:
+    filename: str. Path to FASTA file. Fasta header input string. Format of
+      sequence headers is like >accession="A0A0PX123"   labels="PF00001,PF00002"
+
+  Returns:
+    pd.DataFrame with columns sequence_name (str), sequence (str),
+    true_label(Set[str]).
+  """
+  dict_for_making_df = {'sequence_name': [], 'true_label': [], 'sequence': []}
+
+  with tf.io.gfile.GFile(filename) as f:
+    for line in tqdm.tqdm(f, position=0):
+      line = six.ensure_str(line)
+      if line.startswith('>'):
+        dict_for_making_df['sequence_name'].append(
+            _get_sequence_name_from_sequence_header(line))
+        dict_for_making_df['true_label'].append(
+            _get_labels_from_sequence_header(line))
+      else:
+        dict_for_making_df['sequence'].append(line.rstrip())
+
+  all_test_seqs = pd.DataFrame(dict_for_making_df)
+
+  return all_test_seqs
+
+
+def _set_predicted_labels_missing(row):
+  """Adds empty set as predicted labels for a DataFrame row."""
+  if row[_MERGE_COL_NAME] == 'right_only':
+    return set()
+  else:
+    return row['predicted_labels']
+
+
+def _fillna(df, column_name, value):
+  """Replacement for df.fillna that works with non-indexable values.
+
+  https://github.com/pandas-dev/pandas/issues/21329
+
+  Args:
+    df: pd.DataFrame with column `column_name`.
+    column_name: str.
+    value: value to set when column's value is NaN.
+  """
+  df[column_name] = [
+      (x if isinstance(x, (set, frozenset)) else value) for x in df[column_name]
+  ]
+
+
+def _blast_row_to_confidence_array(
+    row,
+    label_vocab,
+    label_to_vocab_index,
+):
+  """Returns a confidence array of preds (predicted labels get bit_score)."""
+  arr = np.zeros_like(label_vocab, np.float32)
+  indexes_to_update = np.array(
+      [label_to_vocab_index[l] for l in row.predicted_label], dtype=np.int32)
+  arr[indexes_to_update] = row.bit_score
+  return arr
+
+
+def load_blast_output(filename, label_vocab,
+                      training_data_ground_truth,
+                      test_data_ground_truth):
+  """Load a file containing tsv-separated blast output.
+
+  Args:
+    filename: str. Path to output of blast.
+    label_vocab: np.array of string (labels). Used to compute the output column
+      `predictions`.
+    training_data_ground_truth: pd.DataFrame. The output of `load_ground_truth`.
+      Used to compute predicted labels, by using the labels on the training data
+      for the best match, given by blast.
+    test_data_ground_truth: pd.DataFrame. The output of `load_ground_truth`.
+      Used to compute the true labels.
+
+  Returns:
+    pd.DataFrame with columns
+    sequence_name (str);
+    true_label (Set[str]);
+    predicted_label (Set[str]);
+    predictions (np.array of length len(label_vocab), filled with the bit score
+      of the closest match). A value of 0 is used for sequences with no blast
+      calls;
+    percent_seq_identity (float, between 0 and 100);
+    e_value (float, a measure of confidence. Lower is more confident.).
+      A value of NaN is used for missing blast calls;
+    bit_score (float, a measure of confidence. Higher is more confident.).
+      A value of 0 is used for sequences with no blast calls.
+
+    For more information on e-value and bit score, see
+    http://www.metagenomics.wiki/tools/blast/evalue
+  """
+  training_data_ground_truth_lookup = dict(
+      list(
+          zip(training_data_ground_truth.sequence_name,
+              training_data_ground_truth.true_label)))
+
+  blast_output_namedtuples = []
+  with tf.io.gfile.GFile(filename) as f:
+    for line in tqdm.tqdm(f, position=0):
+      blast_output_namedtuples.append(
+          BlastResult.from_string(
+              line, training_data_ground_truth_lookup))
+
+  blast_output = pd.DataFrame.from_records(
+      blast_output_namedtuples, columns=BlastResult._fields)
+
+  # Add in ground truth to get all sequences (including those missed by BLAST).
+  blast_output = blast_output.merge(
+      test_data_ground_truth,
+      on='sequence_name',
+      how='right',
+      indicator=_MERGE_COL_NAME)
+
+  label_to_vocab_index = {l: i for i, l in enumerate(label_vocab)}
+  # For all sequences for which we don't have predictions, set the predictions
+  # to the empty set.
+  _fillna(blast_output, 'predicted_label', frozenset())
+
+  # A bit score of 0 is a "zero confidence" score.
+  blast_output['bit_score'].fillna(0., inplace=True)
+
+  blast_output['predictions'] = blast_output.apply(
+      axis='columns',
+      func=lambda row: _blast_row_to_confidence_array(  # pylint: disable=g-long-lambda
+          row, label_vocab, label_to_vocab_index))
+
+  # Clean up output columns.
+  blast_output.drop(inplace=True, columns=[_MERGE_COL_NAME, 'sequence'])
+
+  return blast_output
+
+
+def _pad_ec_label_with_hyphens(label):
+  """E.g. Given 'EC:1', returns 'EC:1.-.-.-'."""
+  to_return = label
+  while to_return.count('.') != 3:
+    to_return += '.-'
+  return to_return
+
+
+def limit_set_of_labels(df, acceptable_labels,
+                        column_to_limit):
+  """Limits the set of things in df[column_to_limit to acceptable_labels."""
+  working_df = df.copy()
+
+  working_df[column_to_limit] = working_df[column_to_limit].apply(
+      acceptable_labels.intersection)
+  return working_df
+
+
+def limit_labels_for_label_normalizer(
+    label_normalizer,
+    acceptable_labels):
+  """Limits keys and values in label_normalizer to acceptable labels."""
+  limited_label_normalizer = {}
+  for k, v in label_normalizer.items():
+    if k in acceptable_labels:
+      limited_label_normalizer[k] = sorted(
+          acceptable_labels.intersection(set(v)))
+
+  return limited_label_normalizer
+
+
+def load_interproscan_output(
+    interproscan_output_filename,
+    test_data_ground_truth,
+):
+  """Loads tab-separated output from interproscan.
+
+  Args:
+    interproscan_output_filename: file path to tsv interproscan output.
+    test_data_ground_truth: pd.DataFrame. The output of `load_ground_truth`.
+      Used to compute the true labels.
+
+  Returns:
+    pd.DataFrame with output columns 'sequence_name' (Text), predicted_label
+    (frozenset).
+  """
+  interproscan_output_namedtuples = []
+  with tf.io.gfile.GFile(interproscan_output_filename) as f:
+    for line in tqdm.tqdm(f, position=0):
+      interproscan_output_namedtuples.append(
+          InterproScanResult.from_string(line))
+
+  interproscan_output = pd.DataFrame.from_records(
+      interproscan_output_namedtuples, columns=InterproScanResult._fields)
+
+  collapsed_interproscan_output_dict = {
+      'sequence_name': [],
+      'predicted_label': []
+  }
+
+  # There is more than one output per accession because interproscan supports
+  # many analyses of each sequence, and so all the go labels need to be
+  # collapsed.
+  for grouping_key, group in tqdm.tqdm(
+      interproscan_output.groupby('sequence_name'), position=0):
+    collapsed_interproscan_output_dict['sequence_name'].append(grouping_key)
+
+    collapsed_interproscan_output_dict['predicted_label'].append(
+        frozenset().union(*group.predicted_label.values.tolist()))
+
+  collapsed_interproscan_output = pd.DataFrame(
+      collapsed_interproscan_output_dict)
+
+  # Add in ground truth to get all sequences (including those missed by BLAST).
+  interproscan_output = collapsed_interproscan_output.merge(
+      test_data_ground_truth,
+      on='sequence_name',
+      how='right',
+      indicator=_MERGE_COL_NAME)
+
+  # For all sequences for which we don't have predictions, set the predictions
+  # to the empty set.
+  _fillna(interproscan_output, 'predicted_label', frozenset())
+
+  # Clean up output columns.
+  interproscan_output.drop(inplace=True, columns=[_MERGE_COL_NAME, 'sequence'])
+
+  return interproscan_output
---
+++ baseline_utils_test.py
@@ -0,0 +1,324 @@
+# coding=utf-8
+# Copyright 2020 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python2, python3
+# pylint: disable=line-too-long
+"""Tests for module model_performance_analysis.py."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tempfile
+
+from absl.testing import absltest
+from absl.testing import parameterized
+import numpy as np
+import pandas as pd
+
+import baseline_utils
+import test_util
+import tensorflow.compat.v1 as tf
+
+
+def _write_to_file(contents):
+  tmpfile_name = tempfile.mktemp()
+  with tf.io.gfile.GFile(tmpfile_name, "w") as f:
+    f.write(contents.encode("utf-8"))
+  return tmpfile_name
+
+
+class BaselineUtilsTest(parameterized.TestCase):
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name="has fasta character >",
+          header='>accession="ACCESSION"\tlabels="label1,label2"',
+          expected="ACCESSION",
+      ),
+      dict(
+          testcase_name="does not have character >",
+          header='accession="ACCESSION"\tlabels="label1,label2"',
+          expected="ACCESSION",
+      ),
+  )
+  def test_get_sequence_name_from_sequence_header(self, header, expected):
+    actual = baseline_utils._get_sequence_name_from_sequence_header(header)
+    self.assertEqual(actual, expected)
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name="two labels",
+          header='>accession="ACCESSION"\tlabels="label1,label2"',
+          expected={"label1", "label2"},
+      ),
+      dict(
+          testcase_name="zero labels",
+          header='>accession="ACCESSION"\tlabels=""',
+          expected=set(),
+      ),
+  )
+  def test_get_labels_from_sequence_header(self, header, expected):
+    actual = baseline_utils._get_labels_from_sequence_header(header)
+    self.assertEqual(actual, expected)
+
+  def test_load_ground_truth(self):
+    input_fasta = ('>accession="ACCESSION"\tlabels="GO:101010,EC:9.9.9.9"\n'
+                   "ADE\n"
+                   '>accession="ACCESSION2"\tlabels="EC:1.2.-.-"\n'
+                   "WWWW\n")
+    tmpfile_name = _write_to_file(input_fasta)
+    actual = baseline_utils.load_ground_truth(tmpfile_name)
+
+    expected = pd.DataFrame({
+        "sequence_name": ["ACCESSION", "ACCESSION2"],
+        "true_label": [{"GO:101010", "EC:9.9.9.9"}, {"EC:1.2.-.-"}],
+        "sequence": ["ADE", "WWWW"]
+    })
+
+    test_util.assert_dataframes_equal(self, actual, expected)
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name="no inputs, one thing in vocab",
+          input_row=pd.Series({
+              "predicted_label": frozenset([]),
+              "bit_score": 99.
+          }),
+          input_label_vocab=np.array(["PF00001"]),
+          expected=[0.],
+      ),
+      dict(
+          testcase_name="one input, one thing in vocab",
+          input_row=pd.Series({
+              "predicted_label": frozenset(["PF00001"]),
+              "bit_score": 99.
+          }),
+          input_label_vocab=np.array(["PF00001"]),
+          expected=[99.],
+      ),
+      dict(
+          testcase_name="one input, two things in vocab",
+          input_row=pd.Series({
+              "predicted_label": frozenset(["PF00001"]),
+              "bit_score": 99.
+          }),
+          input_label_vocab=np.array(["PF00001", "PF99999"]),
+          expected=[99., 0.],
+      ),
+      dict(
+          testcase_name="two inputs, two things in vocab",
+          input_row=pd.Series({
+              "predicted_label": frozenset(["PF00001", "PF99999"]),
+              "bit_score": 99.
+          }),
+          input_label_vocab=np.array(["PF00001", "PF99999"]),
+          expected=[99., 99.],
+      ),
+  )
+  def test_blast_row_to_confidence_array(self, input_row, input_label_vocab,
+                                         expected):
+    lookup = {k: i for i, k in enumerate(input_label_vocab)}
+
+    actual = baseline_utils._blast_row_to_confidence_array(
+        input_row, input_label_vocab, lookup)
+    np.testing.assert_allclose(actual, expected)
+
+  def test_load_blast_output(self):
+    input_test_fasta = (
+        '>accession="ACCESSION"\tlabels="GO:101010,EC:9.9.9.9"\n'
+        "ADE\n"
+        '>accession="ACCESSION2"\tlabels="EC:1.2.-.-"\n'
+        "WWWW\n")
+    test_fasta_filename = _write_to_file(input_test_fasta)
+    ground_truth_test = baseline_utils.load_ground_truth(test_fasta_filename)
+
+    input_train_fasta = (
+        '>accession="MATCHACCESSION"\tlabels="GO:101010,EC:9.9.9.9,Pfam:PF12345"\n'
+        "ADE\n")
+    train_fasta_filename = _write_to_file(input_train_fasta)
+    ground_truth_train = baseline_utils.load_ground_truth(train_fasta_filename)
+
+    # Missing second sequence in ground truth.
+    input_blast = (
+        'accession="ACCESSION"\taccession="MATCHACCESSION"\t82.456\t57\t10\t0\t1\t57\t1\t57\t6.92e-21\t79.3\n'
+    )
+    input_label_vocab = np.array(
+        ["EC:1.2.-.-", "EC:9.9.9.9", "GO:101010", "Pfam:PF12345"])
+    blast_filename = _write_to_file(input_blast)
+    actual = baseline_utils.load_blast_output(
+        filename=blast_filename,
+        label_vocab=input_label_vocab,
+        test_data_ground_truth=ground_truth_test,
+        training_data_ground_truth=ground_truth_train)
+
+    expected = pd.DataFrame({
+        "sequence_name": ["ACCESSION", "ACCESSION2"],
+        "true_label": [{"GO:101010", "EC:9.9.9.9"}, {"EC:1.2.-.-"}],
+        "predicted_label": [{"GO:101010", "EC:9.9.9.9", "Pfam:PF12345"},
+                            frozenset()],
+        "percent_seq_identity": [82.456, float("nan")],
+        "e_value": [6.92e-21, float("nan")],
+        "bit_score": [79.3, 0.0],
+    })
+
+    test_util.assert_dataframes_equal(
+        self,
+        # Assert dataframes equal except for predictions column.
+        # Rely on unit testing for predictions column instead to increase
+        # test clarity. See test_blast_row_to_confidence_array above.
+        actual.drop(columns=["predictions"]),
+        expected,
+        nan_equals_nan=True)
+
+  def test_limit_set_of_labels(self):
+    # Set up input data.
+    input_df = pd.DataFrame(
+        {"labels": [frozenset(["a"]), frozenset(["a", "b"])]})
+    acceptable_labels = frozenset(["a"])
+    column_to_limit = "labels"
+
+    # Assert input dataframe was not modified later on, so save a copy.
+    input_df_copy = input_df.copy()
+
+    # Compute actual.
+    actual = baseline_utils.limit_set_of_labels(input_df, acceptable_labels,
+                                                column_to_limit)
+    expected = pd.DataFrame({"labels": [frozenset(["a"]), frozenset(["a"])]})
+
+    # Test assertions.
+    test_util.assert_dataframes_equal(self, actual, expected)
+
+    # Assert input dataframe was not modified.
+    test_util.assert_dataframes_equal(self, input_df, input_df_copy)
+
+  def test_limit_labels_for_label_normalizer(self):
+    input_label_normalizer = {
+        "a": ["a", "b", "c"],
+        "DDDD": ["XXXX"],
+        "b": ["YYYY", "b"]
+    }
+    input_acceptable_labels = frozenset(["a", "b"])
+
+    actual = baseline_utils.limit_labels_for_label_normalizer(
+        input_label_normalizer, input_acceptable_labels)
+    expected = {"a": ["a", "b"], "b": ["b"]}
+
+    self.assertDictEqual(actual, expected)
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name="one sequence, one label row, no extra nonlabel entries",
+          interproscan_output="""accession="B7UIV5"	74c763abf8567dfb6f4f83a4e0a31454	217	TIGRFAM	TIGR00506	ribB: 3,4-dihydroxy-2-butanone-4-phosphate synthase	13	209	1.5E-86	T	21-10-2019	IPR000422	3,4-dihydroxy-2-butanone 4-phosphate synthase, RibB	GO:0008686|GO:0009231""",
+          input_ground_truth_test_fasta=""">accession="B7UIV5"\tlabels="GO:101010"
+NOT_USED""",
+          expected_predicted_labels_per_seq={
+              "B7UIV5": {"GO:0008686", "GO:0009231"}
+          },
+      ),
+      dict(
+          testcase_name="one sequence, one label row, extra nonlabel entries",
+          interproscan_output="""accession="B7UIV5"	74c763abf8567dfb6f4f83a4e0a31454	217	TIGRFAM	TIGR00506	ribB: 3,4-dihydroxy-2-butanone-4-phosphate synthase	13	209	1.5E-86	T	21-10-2019	IPR000422	3,4-dihydroxy-2-butanone 4-phosphate synthase, RibB	GO:0008686|GO:0009231
+accession="B7UIV5"	74c763abf8567dfb6f4f83a4e0a31454	217	Gene3D	G3DSA:3.90.870.10		1	217	5.1E-95T21-10-2019""",
+          input_ground_truth_test_fasta=""">accession="B7UIV5"\tlabels="GO:101010"
+NOT_USED""",
+          expected_predicted_labels_per_seq={"B7UIV5": {"GO:0008686", "GO:0009231"}},
+      ),
+      dict(
+          testcase_name="one sequence, no labels",
+          interproscan_output="""accession="B7UIV5"	74c763abf8567dfb6f4f83a4e0a31454	217	Gene3D	G3DSA:3.90.870.10		1	217	5.1E-95T21-10-2019""",
+          input_ground_truth_test_fasta=""">accession="B7UIV5"\tlabels="GO:101010"
+NOT_USED""",
+          expected_predicted_labels_per_seq={"B7UIV5": set()},
+      ),
+      dict(
+          testcase_name="one sequence, multiple label rows, extra nonlabel entries",
+          interproscan_output="""accession="B7UIV5"	74c763abf8567dfb6f4f83a4e0a31454	217	PANTHER	PTHR21327:SF38		1	217	8.2E-126	T21-10-2019
+accession="B7UIV5"	74c763abf8567dfb6f4f83a4e0a31454	217	TIGRFAM	TIGR00506	ribB: 3,4-dihydroxy-2-butanone-4-phosphate synthase	13	209	1.5E-86	T	21-10-2019	IPR000422	3,4-dihydroxy-2-butanone 4-phosphate synthase, RibB	GO:0008686|GO:0009231
+accession="B7UIV5"	74c763abf8567dfb6f4f83a4e0a31454	217	Hamap	MF_00180	3,4-dihydroxy-2-butanone 4-phosphate synthase [ribB].	11	213	43.238	T	21-10-2019	IPR000422	3,4-dihydroxy-2-butanone 4-phosphate synthase, RibB	GO:0008686|GO:0009231
+accession="B7UIV5"	74c763abf8567dfb6f4f83a4e0a31454	217	Gene3D	G3DSA:3.90.870.10		1	217	5.1E-95T21-10-2019
+accession="B7UIV5"	74c763abf8567dfb6f4f83a4e0a31454	217	SUPERFAMILY	SSF55821		7	213	5.95E-86T	21-10-2019	IPR017945	DHBP synthase RibB-like alpha/beta domain superfamily
+accession="B7UIV5"	74c763abf8567dfb6f4f83a4e0a31454	217	Pfam	PF00926	3,4-dihydroxy-2-butanone 4-phosphate synthase	17	208	1.7E-82	T	21-10-2019	IPR000422	3,4-dihydroxy-2-butanone 4-phosphate synthase, RibB	GO:0008686|GO:0009231
+accession="B7UIV5"	74c763abf8567dfb6f4f83a4e0a31454	217	PANTHER	PTHR21327		1	217	8.2E-126	T21-10-2019""",
+          input_ground_truth_test_fasta=""">accession="B7UIV5"\tlabels="GO:101010"
+NOT_USED""",
+          expected_predicted_labels_per_seq={"B7UIV5": {"GO:0008686", "GO:0009231"}},
+      ),
+      dict(
+          testcase_name="two sequences, one has labels",
+          interproscan_output="""accession="B7UIV5"	74c763abf8567dfb6f4f83a4e0a31454	217	PANTHER	PTHR21327		1	217	8.2E-126	T21-10-2019
+    accession="Q5SMK6"	e9a286a263b71156fcf0cfebc12caec6	360	CDD	cd00143	PP2Cc	64	325	6.91138E-87	T	21-10-2019	IPR001932	PPM-type phosphatase domain	GO:0003824""",
+          input_ground_truth_test_fasta=""">accession="B7UIV5"\tlabels="GO:101010"
+NOT_USED
+>accession="Q5SMK6"\tlabels="GO:101010"
+NOT_USED""",
+          expected_predicted_labels_per_seq={
+              "B7UIV5": set(),
+              "Q5SMK6": {"GO:0003824"},
+          },
+      ),
+      dict(
+          testcase_name="two sequences, both have labels",
+          interproscan_output="""accession="B7UIV5"	74c763abf8567dfb6f4f83a4e0a31454	217	TIGRFAM	TIGR00506	ribB: 3,4-dihydroxy-2-butanone-4-phosphate synthase	13	209	1.5E-86	T	21-10-2019	IPR000422	3,4-dihydroxy-2-butanone 4-phosphate synthase, RibB	GO:0008686|GO:0009231
+    accession="Q5SMK6"	e9a286a263b71156fcf0cfebc12caec6	360	CDD	cd00143	PP2Cc	64	325	6.91138E-87	T	21-10-2019	IPR001932	PPM-type phosphatase domain	GO:0003824""",
+          input_ground_truth_test_fasta=""">accession="B7UIV5"\tlabels="GO:101010"
+NOT_USED
+>accession="Q5SMK6"\tlabels="GO:101010"
+NOT_USED""",
+          expected_predicted_labels_per_seq={
+              "B7UIV5": {"GO:0008686", "GO:0009231"},
+              "Q5SMK6": {"GO:0003824"},
+          },
+      ),
+      dict(
+          testcase_name="sequence in ground truth that is missing in interproscan output",
+          interproscan_output="",
+          input_ground_truth_test_fasta=""">accession="B7UIV5"\tlabels="GO:101010"
+NOT_USED""",
+          expected_predicted_labels_per_seq={
+              "B7UIV5": set(),
+          },
+      ),
+  )
+  def test_load_interproscan_output(self, interproscan_output,
+                                    input_ground_truth_test_fasta,
+                                    expected_predicted_labels_per_seq):
+    # Set up inputs.
+    input_file = _write_to_file(interproscan_output)
+
+    input_test_fasta_filename = _write_to_file(input_ground_truth_test_fasta)
+
+    input_ground_truth_test = baseline_utils.load_ground_truth(
+        input_test_fasta_filename)
+
+    # Compute actual results.
+    actual_interproscan_output = baseline_utils.load_interproscan_output(
+        test_data_ground_truth=input_ground_truth_test,
+        interproscan_output_filename=input_file)
+
+    # Assertions.
+    expected_df_length = len(
+        set(
+            list(expected_predicted_labels_per_seq.keys()) +
+            input_ground_truth_test.sequence_name.values))
+    self.assertLen(actual_interproscan_output, expected_df_length)
+
+    for row in actual_interproscan_output.itertuples():
+      self.assertIn(row.sequence_name, expected_predicted_labels_per_seq)
+      self.assertSetEqual(row.predicted_label,
+                          expected_predicted_labels_per_seq[row.sequence_name])
+
+
+if __name__ == "__main__":
+  absltest.main()
