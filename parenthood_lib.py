1d41f8098827b823445e32e5602ba4dae43bf940
Theo Sanderson <theo@theo.io>
2020-07-30 14:31:47 +0100

    Strip out Pfam and fix decode() issues

--- parenthood_lib.py
+++ parenthood_lib.py
@@ -31,7 +31,7 @@ import tqdm
 
 
 # From ftp://ftp.expasy.org/databases/enzyme/enzyme.dat
-DATA_DIR = ''
+DATA_DIR = 'testdata/'
 EC_LEAF_NODE_METADATA_PATH = os.path.join(DATA_DIR, 'enzyme.dat')
 
 # From ftp://ftp.expasy.org/databases/enzyme/enzclass.txt
