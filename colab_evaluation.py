c371ab533cfaf82e2c4efbcd64f1b760a29d7e0f
Theo Sanderson <theo@sndrsn.co.uk>
2020-08-27 15:06:19 +0100

    Update colab eval

--- colab_evaluation.py
+++ colab_evaluation.py
@@ -1,13 +1,31 @@
+# coding=utf-8
+# Copyright 2020 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Version of evaluation utilities intended for use in lower memory environments, such as colab."""
+
 import numpy as np
 import pandas as pd
+import sklearn
 import tqdm
 
 import inference
 import utils
 import evaluation
 
-def batch_inferences(iterator,batch_size):
-    """Yield batches of seq_ids and predictions matrix from an iterator."""
+
+def batch_inferences(iterator, batch_size):
+    """Yield batches of seq_ids and predictions matrices from an iterator."""
     counter = 0
     predictions = []
     seq_ids = []
@@ -20,54 +38,74 @@ def batch_inferences(iterator,batch_size):
         seq_ids.append(inference[0])
         predictions.append(inference[1])
         counter += 1
-        if counter==batch_size:
+        if counter == batch_size:
             yield seq_ids, np.vstack(predictions)
             predictions = []
             seq_ids = []
-            counter=0
+            counter = 0
+
 
 def batched_inferences_from_files(shard_names, batch_size=100):
-    for file_name in tqdm.tqdm(shard_names,position=0):
+    """Iterate through TFRecord files of inferences and yield batches."""
+    for file_name in tqdm.tqdm(shard_names, position=0):
         inference_iterator = inference.parse_shard(file_name)
-        batched_iterator = batch_inferences(inference_iterator,batch_size)
+        batched_iterator = batch_inferences(inference_iterator, batch_size)
         while True:
             try:
                 yield next(batched_iterator)
             except StopIteration:
                 break
 
-def batched_inferences_from_dir(shard_dir_path,batch_size=100):
+
+def batched_inferences_from_dir(shard_dir_path, batch_size=100):
+    """Iterate through directory of inference TFRecord files and yield batches."""
     files_to_process = utils.absolute_paths_of_files_in_dir(shard_dir_path)
     return batched_inferences_from_files(files_to_process, batch_size)
 
 
-def make_tidy_df_from_seq_names_and_prediction_array(sequence_names,predictions_array, vocab, min_decision_threshold=1e-20):
+def make_tidy_df_from_seq_names_and_prediction_array(
+        sequence_names,
+        predictions_array,
+        vocab,
+        min_decision_threshold=1e-20):
+    """Given a list of sequences and a matrix of prediction values, yield a tidy dataframe of predictions."""
     up_ids = []
     labels = []
     values = []
 
     for i in range(len(sequence_names)):
         up_id = sequence_names[i]
-        preds = predictions_array[i,:]
+        preds = predictions_array[i, :]
 
-        for vocab_index in np.argwhere(preds>min_decision_threshold):
-            vocab_index= vocab_index[0]
+        for vocab_index in np.argwhere(preds > min_decision_threshold):
+            vocab_index = vocab_index[0]
             up_ids.append(up_id)
             labels.append(vocab[vocab_index])
             values.append(preds[vocab_index])
     return pd.DataFrame({"up_id": up_ids, "label": labels, "value": values})
 
 
-def get_normalized_inference_results(shard_dir_path, vocab, label_normalizer,min_decision_threshold=1e-20):
+def get_normalized_inference_results(shard_dir_path,
+                                     vocab,
+                                     label_normalizer,
+                                     min_decision_threshold=1e-20):
+    """Take a directory of sharded inferences and output a tidy and normalized dataframe."""
     batches = batched_inferences_from_dir(shard_dir_path)
     dfs = []
     for seq_names, confidences in batches:
-        normed_confidences = evaluation.normalize_confidences(confidences,vocab,label_normalizer)
-        dfs.append(make_tidy_df_from_seq_names_and_prediction_array(seq_names,normed_confidences,vocab,min_decision_threshold=min_decision_threshold) )
+        normed_confidences = evaluation.normalize_confidences(
+            confidences, vocab, label_normalizer)
+        dfs.append(
+            make_tidy_df_from_seq_names_and_prediction_array(
+                seq_names,
+                normed_confidences,
+                vocab,
+                min_decision_threshold=min_decision_threshold))
     return pd.concat(dfs)
 
 
 def make_tidy_df_from_ground_truth(ground_truth):
+    """Create a tidy dataframe from ground truth data."""
     up_ids = []
     labels = []
 
@@ -78,59 +116,92 @@ def make_tidy_df_from_ground_truth(ground_truth):
             labels.append(vocab_entry)
     return pd.DataFrame({"up_id": up_ids, "label": labels, "gt": True})
 
+
 def merge_predictions_and_ground_truth(predictions_df, ground_truth_df):
-    """Performs inner join of predictions and ground truth, then sets all empty values to False."""
-    combined = predictions_df.merge(
-        ground_truth_df,
-        how="outer",
-        suffixes=("_pred", "_gt"),
-        left_on=["label", "up_id"],
-        right_on=["label", "up_id"])
+    """Perform an inner join of predictions and ground truth, then set all empty values to False."""
+    combined = predictions_df.merge(ground_truth_df,
+                                    how="outer",
+                                    suffixes=("_pred", "_gt"),
+                                    left_on=["label", "up_id"],
+                                    right_on=["label", "up_id"])
     combined = combined.fillna(False)
     return combined
 
-def get_pr_curve_df(predictions_df,ground_truth_df, grouping = None, filtered=True):
-  combined = merge_predictions_and_ground_truth(predictions_df,ground_truth_df)
-  if grouping == None:
-    to_process = {'all':combined}.items()
-  else:
-    combined['group'] = combined['label'].map(grouping)
-    to_process = combined.groupby('group')
 
-  del combined
-  output_dfs = []
-  for group_name,group in tqdm.tqdm(to_process,position=0):
-    precisions, recalls, thresholds = sklearn.metrics.precision_recall_curve(group['gt'],group['value'])
-    precisions = precisions[:-1]
-    recalls = recalls[:-1]
-    if filtered:
-      precisions, recalls, thresholds = filter_pr_curve(precisions, recalls, thresholds,1e-3)
-    output_dfs.append(pd.DataFrame({'group':group_name,'precision':precisions, 'recall':recalls, 'threshold':thresholds ,'f1': 2*precisions*recalls/(precisions+recalls)}))
-  return pd.concat(output_dfs)
+def get_pr_curve_df(predictions_df,
+                    ground_truth_df,
+                    grouping=None,
+                    filtered=True):
+    """Given predictions and ground truth in tidy format, yield a precision recall curve.
+    
+    Args:
+        predictions_df: predictions in tidy format
+        ground_truth_df: ground truth in tidy format
+        grouping: optional dictionary mapping sequence names to categories
+        filtered: whether to remove almost redundant points on PR curve
+    """
+    combined = merge_predictions_and_ground_truth(predictions_df,
+                                                  ground_truth_df)
+    if grouping == None:
+        to_process = {'all': combined}.items()
+    else:
+        combined['group'] = combined['label'].map(grouping)
+        to_process = combined.groupby('group')
+
+    del combined
+    output_dfs = []
+    for group_name, group in tqdm.tqdm(to_process, position=0):
+        precisions, recalls, thresholds = sklearn.metrics.precision_recall_curve(
+            group['gt'], group['value'])
+        precisions = precisions[:-1]
+        recalls = recalls[:-1]
+        if filtered:
+            precisions, recalls, thresholds = filter_pr_curve(
+                precisions, recalls, thresholds)
+        output_dfs.append(
+            pd.DataFrame({
+                'group':
+                group_name,
+                'precision':
+                precisions,
+                'recall':
+                recalls,
+                'threshold':
+                thresholds,
+                'f1':
+                2 * precisions * recalls / (precisions + recalls)
+            }))
+    return pd.concat(output_dfs)
 
 
-def filter_pr_curve(precisions,recalls,thresholds,resolution):
-  """Filters out imperceptible shifts in PR curve."""
-  last_precision = None
-  last_recall = None
-  new_precisions = []
-  new_recalls = []
-  new_thresholds = []
-  for i in range(len(precisions)): 
-    if last_precision is None or abs(precisions[i]-last_precision)>=resolution or abs(recalls[i]-last_recall)>=resolution:
-      new_precisions.append(precisions[i])
-      last_precision=precisions[i]
-      new_recalls.append(recalls[i])
-      last_recall=recalls[i]
-      new_thresholds.append(thresholds[i])
-  return np.array(new_precisions), np.array(new_recalls), np.array(new_thresholds)
-  
+def filter_pr_curve(precisions, recalls, thresholds, resolution=1e-3):
+    """Filters out imperceptible shifts in a PR curve."""
+    last_precision = None
+    last_recall = None
+    new_precisions = []
+    new_recalls = []
+    new_thresholds = []
+    for i in range(len(precisions)):
+        if last_precision is None or abs(precisions[i] -
+                                         last_precision) >= resolution or abs(
+                                             recalls[i] -
+                                             last_recall) >= resolution:
+            new_precisions.append(precisions[i])
+            last_precision = precisions[i]
+            new_recalls.append(recalls[i])
+            last_recall = recalls[i]
+            new_thresholds.append(thresholds[i])
+    return np.array(new_precisions), np.array(new_recalls), np.array(
+        new_thresholds)
+
+
 def assign_tp_fp_fn(predictions_df, ground_truth_df, threshold=0.5):
     """Assign each row of a predictions_df as either a TP,FP or FN."""
-    combined = merge_predictions_and_ground_truth(predictions_df, ground_truth_df)
+    combined = merge_predictions_and_ground_truth(predictions_df,
+                                                  ground_truth_df)
 
     combined['tp'] = (combined['gt'] == True) & (combined['value'] > threshold)
-    combined['fp'] = (combined['gt'] == False) & (combined['value'] >
-                                                  threshold)
+    combined['fp'] = (combined['gt']
+                      == False) & (combined['value'] > threshold)
     combined['fn'] = (combined['gt'] == True) & (combined['value'] < threshold)
     return combined
--- colab_evaluation_test.py
+++ colab_evaluation_test.py
@@ -12,8 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""Tests for module inference.py."""
+"""Tests for module colab_evaluation.py."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -22,7 +21,6 @@ import gzip
 import os
 import time
 import math
-
 
 from absl import flags
 from absl.testing import absltest
@@ -44,10 +42,8 @@ class _InferrerFixture(object):
 
 
 class ColabEvaluationTest(parameterized.TestCase):
-
     def setUp(self):
         super(ColabEvaluationTest, self).setUp()
-        initial = './'
 
     def test_parse_sharded_inference_results(self):
         def generate_random_inferences(n):
@@ -55,13 +51,14 @@ class ColabEvaluationTest(parameterized.TestCase):
             accessions_list = []
             activations_list = []
 
-            for i in range(n):
+            for _ in range(n):
                 accession = f"ACCESSION_{time.time()}"
                 activations = np.random.rand(100)
                 accessions_list.append(accession)
                 activations_list.append(activations)
-                serialized_inferences.append(inference.serialize_inference_result(
-                    accession, activations))
+                serialized_inferences.append(
+                    inference.serialize_inference_result(
+                        accession, activations))
 
             return serialized_inferences, accessions_list, activations_list
 
@@ -98,12 +95,12 @@ class ColabEvaluationTest(parameterized.TestCase):
 
         # Check output.
 
-        self.assertEqual(len(actual), math.ceil(num_examples/batch_size))
+        self.assertEqual(len(actual), math.ceil(num_examples / batch_size))
         self.assertEqual(actual[0][0][0], accessions_list[0])
-        self.assertEqual(actual[1][0][1], accessions_list[batch_size+1])
+        self.assertEqual(actual[1][0][1], accessions_list[batch_size + 1])
         np.testing.assert_equal(actual[0][1][0], activations_list[0])
-        np.testing.assert_equal(
-            actual[1][1][1], activations_list[batch_size+1])
+        np.testing.assert_equal(actual[1][1][1],
+                                activations_list[batch_size + 1])
 
     def test_make_tidy_df_from_seq_names_and_prediction_array(self):
         vocab = ["ENTRY0", "ENTRY1", "ENTRY2"]
@@ -111,18 +108,29 @@ class ColabEvaluationTest(parameterized.TestCase):
         predictions_array = np.array([[0.1, 0.9, 0.5], [1, 1, 1]])
         min_decision_threshold = 0.4
         actual_df = colab_evaluation.make_tidy_df_from_seq_names_and_prediction_array(
-            sequence_names, predictions_array, vocab, min_decision_threshold=min_decision_threshold)
-        expected_df = pd.DataFrame({'up_id': ['SEQ0', 'SEQ0', 'SEQ1', 'SEQ1', 'SEQ1'], 'label': [
-                                   'ENTRY1', 'ENTRY2', 'ENTRY0', 'ENTRY1', 'ENTRY2'], 'value': [0.9, 0.5, 1.0, 1.0, 1.0]})
+            sequence_names,
+            predictions_array,
+            vocab,
+            min_decision_threshold=min_decision_threshold)
+        expected_df = pd.DataFrame({
+            'up_id': ['SEQ0', 'SEQ0', 'SEQ1', 'SEQ1', 'SEQ1'],
+            'label': ['ENTRY1', 'ENTRY2', 'ENTRY0', 'ENTRY1', 'ENTRY2'],
+            'value': [0.9, 0.5, 1.0, 1.0, 1.0]
+        })
         pd.testing.assert_frame_equal(actual_df, expected_df)
         return expected_df
 
     def test_make_tidy_df_from_ground_truth(self):
-        input_df = pd.DataFrame({'sequence_name': ['SEQ0', 'SEQ1', 'SEQ2', 'SEQ3'], 'true_label': [
-                                ['ENTRY1'], ['ENTRY1', 'ENTRY2'], [], ['ENTRY6']]})
+        input_df = pd.DataFrame({
+            'sequence_name': ['SEQ0', 'SEQ1', 'SEQ2', 'SEQ3'],
+            'true_label': [['ENTRY1'], ['ENTRY1', 'ENTRY2'], [], ['ENTRY6']]
+        })
         actual_df = colab_evaluation.make_tidy_df_from_ground_truth(input_df)
-        expected_df = pd.DataFrame({'up_id': ['SEQ0', 'SEQ1', 'SEQ1', 'SEQ3'], 'label': [
-                                   'ENTRY1', 'ENTRY1', 'ENTRY2', 'ENTRY6'], 'gt': [True, True, True, True]})
+        expected_df = pd.DataFrame({
+            'up_id': ['SEQ0', 'SEQ1', 'SEQ1', 'SEQ3'],
+            'label': ['ENTRY1', 'ENTRY1', 'ENTRY2', 'ENTRY6'],
+            'gt': [True, True, True, True]
+        })
         pd.testing.assert_frame_equal(actual_df, expected_df)
         return expected_df
 
@@ -131,9 +139,35 @@ class ColabEvaluationTest(parameterized.TestCase):
         pred = self.test_make_tidy_df_from_seq_names_and_prediction_array()
         actual_df = colab_evaluation.merge_predictions_and_ground_truth(
             pred, gt)
-        expected_df = pd.DataFrame({'up_id': ['SEQ0', 'SEQ0', 'SEQ1', 'SEQ1', 'SEQ1', 'SEQ3'], 'label': [
-                                   'ENTRY1', 'ENTRY2', 'ENTRY0', 'ENTRY1', 'ENTRY2', 'ENTRY6'], 'value': [0.9, 0.5, 1.0, 1.0, 1.0, False], 'gt': [True, False, False, True, True, True]})
+        expected_df = pd.DataFrame({
+            'up_id': ['SEQ0', 'SEQ0', 'SEQ1', 'SEQ1', 'SEQ1', 'SEQ3'],
+            'label':
+            ['ENTRY1', 'ENTRY2', 'ENTRY0', 'ENTRY1', 'ENTRY2', 'ENTRY6'],
+            'value': [0.9, 0.5, 1.0, 1.0, 1.0, False],
+            'gt': [True, False, False, True, True, True]
+        })
         pd.testing.assert_frame_equal(actual_df, expected_df)
+
+    def test_get_pr_curve_df(self):
+        gt = self.test_make_tidy_df_from_ground_truth()
+        pred = self.test_make_tidy_df_from_seq_names_and_prediction_array()
+        pr_curve = colab_evaluation.get_pr_curve_df(pred, gt)
+
+        np.testing.assert_almost_equal(pr_curve['recall'],
+                                       np.array([1, 0.75, 0.75, .5]))
+
+    def test_assign_tp_fp_fn(self):
+        gt = self.test_make_tidy_df_from_ground_truth()
+        pred = self.test_make_tidy_df_from_seq_names_and_prediction_array()
+        tp_fp_fn = colab_evaluation.assign_tp_fp_fn(pred, gt)
+
+        expected = pd.DataFrame({
+            'tp': [True, False, False, True, True, False],
+            'fp': [False, False, True, False, False, False],
+            'fn': [False, False, False, False, False, True]
+        })
+        actual = tp_fp_fn.loc[:,[ "tp", "fp", "fn"]]
+        pd.testing.assert_frame_equal(expected, actual)
 
 
 if __name__ == '__main__':
