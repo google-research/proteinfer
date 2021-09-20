46dcd912706d16dd16c5c9a8088eeaf51ef38e3f
theosanderson <theo@theo.io>
2021-09-20 17:56:49 +0100

    Change PDF splitting technique, bump PDF

--- latex/make_tex.sh
+++ latex/make_tex.sh
@@ -2,4 +2,5 @@ pdflatex -interaction nonstopmode final_format.tex
 pdflatex -interaction nonstopmode final_format.tex
 bibtex final_format.aux
 pdflatex -interaction nonstopmode final_format.tex
-pdflatex -interaction nonstopmode supplement.tex
+qpdf final_format.pdf --pages . 1-10 -- main.pdf
+qpdf final_format.pdf --pages . 11-z -- supplement.pdf
