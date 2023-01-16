798f0de25bb2e02326c78facfd898338d7c4fa99
Theo Sanderson <theo@theo.io>
2023-01-16 15:11:15 +0000

    updates

--- latex/make_tex.sh
+++ latex/make_tex.sh
@@ -2,5 +2,5 @@ pdflatex -interaction nonstopmode final_format.tex
 pdflatex -interaction nonstopmode final_format.tex
 bibtex final_format.aux
 pdflatex -interaction nonstopmode final_format.tex
-qpdf final_format.pdf --pages . 1-10 -- main.pdf
-qpdf final_format.pdf --pages . 11-z -- supplement.pdf
+qpdf final_format.pdf --pages . 1-12 -- main.pdf
+qpdf final_format.pdf --pages . 13-z -- supplement.pdf
